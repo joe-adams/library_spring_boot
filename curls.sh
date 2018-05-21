0f86493fbe20de5f5b6c9e09f7af65b631e58447
Your Name <you@example.com>
2018-05-20 22:59:11 -0500

    done

--- curls.sh
+++ curls.sh
@@ -1,5 +1,6 @@
-curl -X GET http://localhost:8080/books/title/dog
-curl -X GET http://localhost:8080/books/author/smith
+#!/bin/bash
+curl -X GET -i http://localhost:8080/books/title/dog
+curl -X GET -i http://localhost:8080/books/author/smith
 
 curl -X POST -i http://localhost:8080/books -H "Content-Type: application/json" --data '{
   "title":"Great book",
